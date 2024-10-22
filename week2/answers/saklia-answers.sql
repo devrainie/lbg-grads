92ea570e58e5eb749971e7ddaca6c63af3978696
devrainie <rainietamakloe@gmail.com>
2024-10-22 15:14:46 +0100

    EDIT: q1-3 answers

---
+++ week2/answers/saklia-answers.sql
@@ -0,0 +1,18 @@
+################## QUESTION 1
+
+select first_name, last_name
+from actor;
+
+################## QUESTION 2
+
+select first_name, last_name
+from actor
+where first_name = "John";
+
+################### QUESTION 3
+
+select first_name, last_name 
+from actor
+where last_name = "Neeson";
+
+################### QUESTION 4
