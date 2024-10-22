80bf3ab9a33738b65fa7b22e4659f002df29dcbf
devrainie <rainietamakloe@gmail.com>
2024-10-22 15:46:19 +0100

    EDIT: Answers Q4-q6

--- week2/answers/saklia-answers.sql
+++ week2/answers/saklia-answers.sql
@@ -16,3 +16,25 @@ from actor
 where last_name = "Neeson";
 
 ################### QUESTION 4
+
+select actor_id, first_name, last_name
+from actor
+where mod(actor_id, 10) = 0;
+
+################### QUESTION 5
+
+select film_id, title, description
+from film
+where film_id = 100;
+#Description is "A Beautiful Drama of a Dentist And a Composer who must Battle a Sumo Wrestler in The First Manned Space Station"
+
+################### QUESTION 6
+
+select film_id, title, rating
+from film
+where rating = "R";
+
+
+
+
+
